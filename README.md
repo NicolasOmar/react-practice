24efcc701cd87d49159ee7473bf60cf2ed8e23ae
Nicolás Omar González Passerino <nicolas.passerino@gmail.com>
2020-03-08 01:53:19 -0300

    Component creation  & composition learning

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # React Practice
 Repository created to record my practice learning React with exercices base on [React Course](https://www.udemy.com/user/sgslo/) of [Stephen Grider](https://www.udemy.com/course/react-redux).
 
+## How to use it
+The first step will be install all the packages using the following command:
+```sh
+npm install
+```
+After the instalation, you can run the app by executing the following command:
+```sh
+npm start
+```
+
 ## Changelog
 | Version | Progress |
 | ------ | ------ |
+| 0.0.3 | [Semantic UI](https://semantic-ui.com/) v2.4.1 added by cdn. [Faker.js](https://www.npmjs.com/package/faker) added to use mocked data. Component creation with data passing using props & composition using props.children. |
 | 0.0.2 | How to use css classes, styles and js variables in jsx |
 | 0.0.1 | React project created with [Create-react-app](https://www.npmjs.com/package/create-react-app) |
--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+// COMPONENTS
+import CommentDetail from './commentDetails';
+import ApprovalCard from './approvalCard';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 2:00PM"
+          comment="Interesting answer"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>      
+      <ApprovalCard>
+        <CommentDetail
+          author="Will"
+          timeAgo="Today at 1:00PM"
+          comment="Good point of view"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Andrew"
+          timeAgo="Yesterday at 3:15PM"
+          comment="I could be better using blue"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  )
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector('#root'));
